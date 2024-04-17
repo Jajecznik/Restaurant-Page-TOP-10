e152a7558d1042cc5c6b5e49c1cb0e7ced5e65ea
Jajecznik <mcjajecznik@gmail.com>
2024-04-17 23:09:40 +0200

    header and footer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+import './styles.css';
+import Icon from './images/icon.png';
+
+const header = document.querySelector('header');
+const logo = document.getElementById("logo");
+
+const imageElement = document.createElement('img');
+imageElement.src = Icon;
+imageElement.alt = "restauront logo";
+
+imageElement.onload = function () {
+    let logoPosition = (header.offsetHeight - logo.offsetHeight) / 2;
+    logo.style.top = `${logoPosition}px`;
+    logo.style.left = `${logoPosition}px`;
+};
+
+logo.appendChild(imageElement);
--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,29 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: "Restaurant Page"
+            template: './src/index.html',
+            favicon: './src/images/icon.png'
         })
     ],
 };
